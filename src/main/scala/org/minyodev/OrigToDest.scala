da73de019535190c4053b2141ff9a9d175e29d5d
aesguerra-c4w <aesguerra-c4w@github.com>
2017-08-16 16:23:07 +0800

    Updated OrigToDest, added more details about the trip

--- src/main/scala/org/minyodev/OrigToDest.scala
+++ src/main/scala/org/minyodev/OrigToDest.scala
@@ -6,28 +6,29 @@ import com.google.maps.{DirectionsApi, GeoApiContext}
 import scala.collection.JavaConversions._
 
 /**
-  * A program that will determine information of going to one place to another
+  * A program that will determine information of going from one place to another
+  *
+  * @author aesguerra
+  * @since  2017-08-16
   */
 object OrigToDest {
 
-  def main(args: Array[String]): Unit = {
-
-    val context: GeoApiContext = new GeoApiContext()
-    context.setApiKey("AIzaSyBlw8S1lyoOf8YniWQREPtpg2UeOPFUVeA")
-    // context.setEnterpriseCredentials("", "") // For premium customers only ;D
-
-    val origin = "Greenbelt Mall, Legazpi Street, Makati, Kalakhang Maynila, Philippines"
-    val destination = "Sun Life Center, 5th Avenue, Rizal Drive, Bonifacio Global City, Taguig, 1634 Metro Manila, Philippines"
+  /**
+    * Get information regarting the trip's routes, distance, time of arrival, etc
+    * @param context Google Map API's GeoApiContext
+    * @param origin A place where a person originates
+    * @param destination A place where a person is going
+    * */
+  def getDirectionInfo(context: GeoApiContext, origin: String, destination: String): Unit = {
 
     val result: DirectionsResult =
       DirectionsApi.getDirections(context, origin, destination).await()
-
-    result.geocodedWaypoints.toList.foreach(geoWaypoint => {
-      println("Geocoder Place ID: [" + geoWaypoint.placeId + "]")
-      println("Geocoder Status: [" + geoWaypoint.geocoderStatus + "]")
-    })
-
-    println()
+    val geoWayPoints = result.geocodedWaypoints.toList.map { geoWaypoint =>
+      "\tPlace ID: " + geoWaypoint.placeId + "\n" +
+      "\tGeocoder Status: " + geoWaypoint.geocoderStatus + "\n" +
+      "\tPartial Match: " + geoWaypoint.partialMatch + "\n" +
+      "\tTypes: " + geoWaypoint.types.toList.map(t => t.toString).mkString("/")
+    } mkString("\n")
 
     result.routes.toList.foreach(r => {
       val legs = r.legs.toList.map { l =>
@@ -41,11 +42,27 @@ object OrigToDest {
 
       val polyline = r.overviewPolyline.decodePath().toList.map(ll => "\t" + ll.lat + "," + ll.lng + "").mkString("\n")
 
-      println("Summary: [" + r.summary + "]")
+      println("Origin: " + origin)
+      println("Destination: " + destination)
+      println("Summary: " + r.summary)
       println("North East: [" + r.bounds.northeast.lat + "," + r.bounds.northeast.lng + "]")
       println("South West: [" + r.bounds.southwest.lat + "," + r.bounds.southwest.lng + "]")
-      println("Legs: [\n" +  legs + "]")
-      println("Polyline: [\n" + polyline + "]")
+      println("Legs:\n" +  legs)
+      println("Geocoded Way Points:\n" + geoWayPoints)
+      println("Polyline:\n" + polyline)
     })
+
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val context: GeoApiContext = new GeoApiContext()
+    context.setApiKey("AIzaSyBlw8S1lyoOf8YniWQREPtpg2UeOPFUVeA")
+    // context.setEnterpriseCredentials("", "") // For premium customers only ;D
+
+    val origin = "Greenbelt Mall, Legazpi Street, Makati, Kalakhang Maynila, Philippines"
+    val destination = "Sun Life Center, 5th Avenue, Rizal Drive, Bonifacio Global City, Taguig, 1634 Metro Manila, Philippines"
+
+    getDirectionInfo(context, origin, destination)
   }
 }
